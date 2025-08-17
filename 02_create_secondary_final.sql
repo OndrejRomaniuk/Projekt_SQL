099504d569e166cc2869bdc4d91b38770b1e11a1
Ondřej Jan Romaniuk <romaniuko73@gmail.com>
2025-08-17 14:26:10 +0200

    Update 02_create_secondary_final.sql

--- 02_create_secondary_final.sql
+++ 02_create_secondary_final.sql
@@ -1,12 +1,6 @@
-
-
 CREATE TABLE t_Ondrej_Romaniuk_project_SQL_secondary_final AS					-- vyselektování dat pro ČR a roky 2006 - 2018
 SELECT *
 FROM economies
 WHERE
     YEAR BETWEEN 2006 AND 2018
     AND country = 'Czech Republic';
-
-SELECT *
-FROM t_Ondrej_Romaniuk_project_SQL_secondary_final;
-		
