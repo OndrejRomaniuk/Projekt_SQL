f6179f7a4a08691a71a780b5fb04ff057cc85634
Ondřej Jan Romaniuk <romaniuko73@gmail.com>
2025-08-20 20:34:34 +0200

    Update Q3_slowest_price_growth.sql

--- Q3_slowest_price_growth.sql
+++ Q3_slowest_price_growth.sql
@@ -6,7 +6,7 @@ FROM t_ondrej_romaniuk_project_sql_primary_final;
 
 CREATE TABLE t_Ondrej_Romaniuk_project_SQL_Q3_slowest_price_growth AS										-- 2. Tabulka s odpovědí
 WITH
-  mezirocni_narust AS (
+mezirocni_narust AS (
     SELECT
         category_code,
         rok,
@@ -18,24 +18,26 @@ WITH
     FROM t_ondrej_romaniuk_project_sql_primary_final
   ),
 
-  procenta AS (
+procenta AS (
     SELECT
-      mn.category_code,
-      mn.rok,
-      COALESCE(mn.mezirocni_narust_procent, 0) || '%' AS procentualni_narust
+    	mn.category_code,
+    	mn.rok,
+    	COALESCE(mn.mezirocni_narust_procent, 0) || '%' AS procentualni_narust
     FROM mezirocni_narust mn
   )
 
 SELECT
-  p.category_code,
-  cpc.name,
-  p.rok,
-  p.procentualni_narust
+	p.category_code,
+	cpc.name,
+	p.rok,
+	p.procentualni_narust
 FROM procenta p
-LEFT JOIN czechia_price_category cpc ON p.category_code = cpc.code
+LEFT JOIN czechia_price_category cpc 
+	ON p.category_code = cpc.code
 ORDER BY CAST(REPLACE(p.procentualni_narust, '%', '') AS NUMERIC) ASC
 LIMIT 1;
 
 
 SELECT *																									-- 3. Volání tabulky
 FROM t_Ondrej_Romaniuk_project_SQL_Q3_slowest_price_growth;
+
